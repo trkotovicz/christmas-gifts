58899588dc68c5543555fe6699c84b6456042c9f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-12-28 18:17:46 -0300

    create app.ts and conection with db

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: [],
+  migrations: ['src/database/migration/*.ts'],
+  // ssl: { rejectUnauthorized: false },
+  subscribers: [],
+});
