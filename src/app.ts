2319dde9775da833252f3081c2dbdd99895a1abb
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-12-29 16:58:43 -0300

    feat: implements error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './middlewares/error';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(errorHandler);
 
 export default app;
---
+++ src/errors/catalog.ts
@@ -0,0 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+
+export enum ErrorTypes {
+  GenericError = 'GenericError',
+  BadRequest = 'BadRequest',
+  UnauthorizedError = 'UnauthorizedError',
+}
+
+interface ErrorResponseObject {
+  message: string;
+  httpStatus: number;
+}
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject;
+};
+
+export const errorCatalog: ErrorCatalog = {
+  GenericError: {
+    message: 'Internal error',
+    httpStatus: StatusCodes.INTERNAL_SERVER_ERROR,
+  },
+  BadRequest: {
+    message: 'Something wrong happened',
+    httpStatus: StatusCodes.BAD_REQUEST,
+  },
+  UnauthorizedError: {
+    message: 'Unauthorized user',
+    httpStatus: StatusCodes.UNAUTHORIZED,
+  }
+};
