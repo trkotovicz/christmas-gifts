8824a8063720efb0ae27ab11b15a3d4714c8b6c2
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-12-29 16:46:16 -0300

    feat: import entities at data-source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,17 +1,19 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { Person } from './entity/Person';
+import { EBook } from './entity/EBook';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.HOST,
-  port: 5432,
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   synchronize: true,
   logging: true,
-  entities: [],
+  entities: [Person, EBook],
   migrations: ['src/database/migration/*.ts'],
   // ssl: { rejectUnauthorized: false },
   subscribers: [],
