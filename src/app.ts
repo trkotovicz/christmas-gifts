2c783f039307164bd54ab94934bd8ff779aa15ab
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-01-05 19:18:29 -0300

    docs: API documentation created

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
 import errorHandler from './middlewares/error';
 import ebookRouter from './routes/Ebook';
-import personRouter from './routes/Person';
 import nodemailerRouter from './routes/NodemailerRouter';
+import personRouter from './routes/Person';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(ebookRouter);
 app.use(personRouter);
